23e6b2d526bea1050ba4708d7c89cc75ed0e7527
ltvl <letranvietlong@gmail.com>
2021-08-08 20:56:21 +0700

    add

---
+++ js/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Em à😘',
+    introDesc: `Màu anh thích là màu xanh dương, còn em mãi là người anh thương🥰`,
+    btnIntro: 'Xem nào!',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'I Love You 3000😘',
+    btnNo: 'Không, anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh 🥰',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    //messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Yêu Anh😘',
+    messLink: 'http://facebook.com/letranvietlong'
+}
